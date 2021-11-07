4f704d633fdc9313ab5e73fc10a62a5ce7ee66c6
Ron Almog <ron8132@gmail.com>
2021-11-07 16:13:37 -0500

    define interface for useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [age, setAge] = useState<number>(30);
+
+  interface IState {
+    people: {
+      name: string;
+      age: number;
+      url: string;
+      note?: string;
+    }[];
+  }
+  const [people, setPeople] = useState<IState["people"]>();
+
+  const changeAge = () => {
+    setAge(77);
+    console.log("age is ", age);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People invited to our party</h1>
     </div>
   );
 }
